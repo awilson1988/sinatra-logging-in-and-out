1b02bd476e808ca585cdd13c59a44d761f57298e
amanda <amanda.blaze1988@gmail.com>
2021-02-16 17:25:48 -0800

    commit tree be1d4efa4a5141c371b383377448e8b2a3427010

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,16 +12,30 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
 
-  end
+    @user = User.find_by(username: params["username"])
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
+  end	  
 
-  get '/account' do
 
-  end
-
-  get '/logout' do
-
-  end
+  get '/account' do	  
+    erb :account
+  end	  
 
 
-end
+  get '/logout' do	  
+    session.clear
+    redirect to '/'
+  end	  
+
+
+
+end	
+
+
+
 
